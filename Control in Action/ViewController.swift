8e0e1f53d19b447be77b74912594330962e40355
dimkasp@mail.ru <dimkasp@mail.ru>
2019-04-13 17:39:52 +0300

    test new action switch text field button

--- Control in Action/ViewController.swift
+++ Control in Action/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func clickBtn(_ sender: UIButton) {
+        print ("нажата кнопка")
+    }
+    
+    
+    @IBAction func mySwitchBtn(_ sender: Any) {
+        if (sender as AnyObject).isOn {
+            print ("Switch is on")
+        } else {
+            print ("Switch is off")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
